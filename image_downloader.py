855c9056d92bd2a8ef6c2b34346c363e5a4f7108
karol <siadajpan@o2.pl>
2019-02-26 11:48:43 +0100

    add all the functions for image processor. add pictures for test images

--- image_downloader.py
+++ image_downloader.py
@@ -1,18 +1,28 @@
 import argparse
 import requests
 from image_utils.image_processor import Image_Processor
-
+import numpy as np
 
 ap = argparse.ArgumentParser(description='Download picture from url and process it.')
+
+# first positional argument will store string image url
 ap.add_argument('image_url', help='URL path to the picture.')
+
+# if user will use -g -> 'gray' parameter will be saved as True
 ap.add_argument('-g', '--gray', action='store_true', 
                 help='change image to gray scale')
+
+# user needs to use e.g. -b 120 to specify binary threshold
+# this value is clipped to [0, 255] during processing
 ap.add_argument('-b', '--binary', type=int, 
                 help='set binary threshold. Integer number 0-255')
+
 ap.add_argument('-rc', '--reverse_colors', action='store_true',
                 help='reverse colors')
+
 ap.add_argument('-r', '--rotate', type=int, choices=[90, 180, 270],
                 help='rotate clockwise by 90, 180 or 270 degrees')
+
 ap.add_argument('-c', '--cut', nargs='+', type=int,
                 help="cut part of picture specified by pixel top left, and bottom right.\
                 Usage example '-c 10, 5, 150, 200' this will cut image part\
@@ -25,16 +35,23 @@ args = ap.parse_args()
 image_processor = Image_Processor()
 image_processor.download_image(args.image_url)
 
-# go through all user-selected options and process image accordingly
+# go through all available options and process image accordingly
+# to the way user has chosen. If user didn't choose anything
+# this will loop 6 times, but image will not be processed
 for argument in vars(args):
 
-    # if the value of argument is not None or False (it was selected by user)
+    # get parameter of the argument
     value = getattr(args, argument)
-    if value:
+    # if the parameter is not None or False -> it was selected by user
+    if value not in [None, False]:
         if argument == 'gray':
             image_processor.to_gray()
             
         elif argument == 'binary':
+            # check if binary threshold is in range 0-255
+            if not 0 <= value <= 255:
+                raise ValueError('binary threshold not in range [0, 255]')
+            
             image_processor.binary_threshold(value)
             
         elif argument == 'reverse_colors':
@@ -45,5 +62,5 @@ for argument in vars(args):
             
         elif argument == 'cut':
             image_processor.cut(value)
-
+    
 image_processor.show_images()
--- image_utils/image_processor.py
+++ image_utils/image_processor.py
@@ -1,25 +1,31 @@
 import cv2
 import requests
-from PIL import Image
 from io import BytesIO
 import numpy as np
+import time
+
 
 class Image_Processor:
     def __init__(self):
         self.original_image = None
         self.processed_image = None
         
+    # download image from url and save it to self.original_image
     def download_image(self, image_url):
         img_stream = BytesIO(requests.get(image_url).content)
         self.original_image = cv2.imdecode(np.fromstring(img_stream.read(), 
                                                          np.uint8), 1)
-        
+    # change color to gray scale
     def to_gray(self):
         self.processed_image = cv2.cvtColor(self.original_image, 
                                             cv2.COLOR_BGR2GRAY)
+        # merge into 3 channels picture, so it can be displayed with color one
+        self.processed_image = cv2.cvtColor(self.processed_image,
+                                            cv2.COLOR_GRAY2BGR)
     
+    # rotate image by one of [90, 180, 270] degrees clockwise
     def rotate(self, rotation):
-        
+        # defy some flipping and transposing functions
         def h_flip(image):
             return cv2.flip(image, 1)
         
@@ -29,6 +35,7 @@ class Image_Processor:
         def transpose(image):
             return cv2.transpose(image)
         
+        # process image depending on user choice
         if rotation == 90:
             self.processed_image = h_flip(transpose(self.original_image))
             
@@ -41,14 +48,127 @@ class Image_Processor:
         else:
             raise ValueError('invalid rotation for image: ', rotation)
     
+    # execute binary threshold. Save image 
     def binary_threshold(self, threshold):
-        pass
+        print(threshold)
+        if not 0 <= threshold <= 255:
+            raise ValueError('binary threshold not in range [0, 255]')
+        
+        # first change the original image to gray scale
+        self.to_gray()
+        # execute binary threshold
+        _, self.processed_image = cv2.threshold(self.processed_image,
+                                             threshold, 255, cv2.THRESH_BINARY)
     
+    # cut image into region specified by pixels
     def cut(self, pixels):
-        pass
+        try:
+            x_start, y_start, x_end, y_end = pixels
+        except TypeError:
+            raise TypeError('too many arguments for cut')
+        except ValueError:
+            raise TypeError('not enough arguments for cut')
+        else:
+            # get widht and height of image and check if region is correct
+            (height, width) = self.original_image.shape[:2]
+            if not 0 <= x_start < x_end <= width or \
+               not 0 <= y_start < y_end <= height:
+                raise ValueError('invalid parameters of cut x: {} y:{} to x: {}, y: {}\
+                for image size width: {}, height: {}'.format(*pixels, width, height))
+            print(pixels)
+            self.processed_image = self.original_image[y_start : y_end, 
+                                                       x_start : x_end]
     
+    # reverse colors of an image using bitwise_not
     def reverse_colors(self):
-        pass
+        self.processed_image = cv2.bitwise_not(self.original_image)
     
+    
+    # private method for creating file names from current time and suffix
+    def _create_file_name(self, suffix):
+        ext = '.png'
+        name = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime()) + suffix + ext
+        return name
+    
+    # private method to add black boarders on top and below an image
+    def _create_higher_image(self, image, border):
+        # border is a float number. It can be either whole number (e.g. 4.0) or 
+        # whole number + 0.5 (e.g. 4.5). If it is whole, both int(border) and
+        # int(border + 0.6) will return the same number (4). If the border is
+        # e.g. 4.5, top border will be 4, and bottom will be int(5.1) = 5
+        return cv2.copyMakeBorder(image, int(border), int(border + 0.6), 
+                                      0, 0, cv2.BORDER_CONSTANT, 0)
+    
+    # private method to create image to display for user. Either original, or
+    # original with processed side-by-side
+    def _create_output_image(self):
+        # show both images side-by-side if there is processed image
+        if self.processed_image is not None:
+            # calculate border size based on differences between heights
+            height_o = self.original_image.shape[0]
+            height_p = self.processed_image.shape[0]
+            border = (max([height_o, height_p]) - min([height_o, height_p]))/2
+    
+            # extend height of one of the images if heights are different
+            if height_o > height_p:
+                new_original = self.original_image
+                new_processed = self._create_higher_image(self.processed_image, 
+                                                          border)
+            else:
+                new_original = self._create_higher_image(self.original_image, 
+                                                         border)
+                new_processed = self.processed_image
+    
+            # concatenate pictures side-by-side
+            out_image = np.concatenate((new_original, new_processed), axis=1)
+            title = 'Image comparison'
+            
+        else:
+            out = self.original_image
+            title = 'Original image'
+            
+        return [title, out_image]
+    
+    
+    # display images and wait for user input for 100 seconds
     def show_images(self):
-        pass
+        
+        # create and display image
+        cv2.imshow(*self._create_output_image())
+        
+        # save pictures as user specify, quit after pressing 'q'
+        while True:
+            # wait for key press
+            key_pressed = cv2.waitKey(0)
+            
+            # user pressed 'o'
+            if key_pressed == ord('o'):
+                self.save_original()
+            
+            # user pressed 'p' and there is a processed image
+            elif key_pressed == ord('p') and self.processed_image is not None:
+                self.save_processed()
+            
+            # user pressed 'b' and there is a processed image
+            elif key_pressed == ord('b') and self.processed_image is not None:
+                cv2.imwrite(self._create_file_name('_b'), out)
+                
+            # user pressed 'q'
+            elif key_pressed == ord('q'):
+                break
+        
+        cv2.destroyAllWindows()
+        
+        
+    def save_original(self):
+        file_name = self._create_file_name('_o')
+        cv2.imwrite(file_name, self.original_image)
+        return file_name
+    
+    
+    def save_processed(self):
+        file_name = self._create_file_name('_p')
+        cv2.imwrite(file_name, self.processed_image)
+        return file_name
+    
+                  
